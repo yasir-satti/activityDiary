c14b6227d2b052881992fbfd0639142a3c3ee6b4
Yasir Satti <yassir_satti@hotmail.com>
2020-03-19 17:38:59 +0000

    add requirements file and systemd service file

--- application/__init__.py
+++ application/__init__.py
@@ -8,10 +8,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 from os import getenv
-app.config['SECRET_KEY'] = getenv('SECRET_KEY_PROJ')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 # configer db connection
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI_PROJ')
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 
 # create db object
 db = SQLAlchemy(app)
