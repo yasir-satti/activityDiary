e0fe29505c35ccc430aedbd8b94d2f662cf90e8d
Yasir Satti <yassir_satti@hotmail.com>
2020-03-20 21:20:10 +0000

    adding activity add form

--- application/routes.py
+++ application/routes.py
@@ -16,7 +16,7 @@ def activityadd():
     form = AddForm()
     if form.validate_on_submit():
         addData = Activities (
-            activitydate=form.activitydate.data,
+            activityDate=form.activitydate.data,
             activityUser=form.user_id.data,
             activityDesc=form.activityDesc.data,
             objRating=form.objRating.data,
