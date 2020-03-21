e7ce9dccc17afeec5e17bcace0ed5b4bca6fbffe
Yasir Satti <yassir_satti@hotmail.com>
2020-03-21 00:36:31 +0000

    added form: display activity

--- application/routes.py
+++ application/routes.py
@@ -58,19 +58,20 @@ def activitydisplay():
 
 @app.route('/activitymd')
 def activitymd():
-    form = ModifyForm()
-    if form.validate_on_submit():
-        current_user.first_name = form.first_name.data
-        current_user.last_name = form.last_name.data
-        current_user.email = form.email.data
-        db.session.commit()
-        return redirect(url_for('home'))
-    elif request.method == 'GET':
-        form.first_name.data = current_user.first_name
-        form.last_name.data = current_user.last_name        
-        form.email.data = current_user.email        
-    return render_template('activitydisplay.html', title='Modify Activity', form=form)
-    
+    #form = ModifyForm()
+    #if form.validate_on_submit():
+    #    current_user.first_name = form.first_name.data
+    ##    current_user.last_name = form.last_name.data
+    #    current_user.email = form.email.data
+    #    db.session.commit()
+    #    return redirect(url_for('home'))
+    #elif request.method == 'GET':
+    #    form.first_name.data = current_user.first_name
+    #    form.last_name.data = current_user.last_name        
+    #    form.email.data = current_user.email        
+    #return render_template('activitydisplay.html', title='Modify Activity', form=form)
+    return render_template('activitymd.html', title='Modify Activity')
+
 @app.route('/activitydelete')
 def activitydelete(): 
     return render_template('activitydelete.html', title='Delete Activity')
