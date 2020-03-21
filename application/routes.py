e0537e85a1f532abe9cf872b7aec7e1bf39fcf43
Yasir Satti <yassir_satti@hotmail.com>
2020-03-21 01:45:11 +0000

    added form: display activity

--- application/routes.py
+++ application/routes.py
@@ -48,8 +48,8 @@ def activityadd():
 
 @app.route('/activitydisplay', methods=['GET', 'POST'])
 def activitydisplay():
-    userData = Users.query.id(1)
-    displayData = Activities.query.user_id(1)
+    userData = Users.query.filter_by(id=1).first()
+    displayData = Activities.query.filter_by(user_id=1).first()
     form = DisplayForm()
     if request.method == 'GET':
         form.first_name.label = userData.first_name
