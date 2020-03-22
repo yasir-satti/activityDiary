8ae04a81816c9f62552745cfa2c659bacd256de6
Yasir Satti <yassir_satti@hotmail.com>
2020-03-22 16:15:06 +0000

    added form: modify activity

--- application/routes.py
+++ application/routes.py
@@ -92,8 +92,45 @@ def activitymd():
     #return render_template('activitydisplay.html', title='Modify Activity', form=form)
     return render_template('activitymd.html', title='Modify Activity')
 
-@app.route('/activitydelete')
-def activitydelete(): 
+@app.route('/activitydelete', methods=['GET', 'POST'])
+def activitydelete():
+    form = DeleteForm()
+    if form.validate_on_submit():
+        # Activities.activityDate=form.activityDate.data
+        Activities.activityDesc=form.activityDesc.data
+        Activities.ObjRating=form.objRating.data
+        Activities.JoyRating=form.joyRating.data  
+        db.session.commit()        
+        return redirect(url_for('home'))
+    elif request.method == 'GET':
+        data = Activities.query.first()
+        #userName = Users.query.filter_by(data.user_id)
+        #user = userName.first_name + " " + userName.last_name
+        user = data.user_id
+        # form.activityUser.data=data.user_id,
+        # form.activityDate.data=data.activityDate,
+        form.activityDesc.data=data.activityDesc,
+        form.objRating.data=data.ObjRating,
+        form.joyRating.data=data.JoyRating
+        return render_template('activitydelete.html', title='Delete Activity', form=form, user=user, data=data)
+    else:
+        print(form.errors)    
+    #form = ModifyForm()
+    #if form.validate_on_submit():
+    #    current_user.first_name = form.first_name.data
+    ##    current_user.last_name = form.last_name.data
+    #    current_user.email = form.email.data
+    #    db.session.commit()
+    #    return redirect(url_for('home'))
+    #elif request.method == 'GET':
+    #    form.first_name.data = current_user.first_name
+    #    form.last_name.data = current_user.last_name        
+    #    form.email.data = current_user.email        
+    #return render_template('activitydisplay.html', title='Modify Activity', form=form)
+    return render_template('activitydelete.html', title='Delete Activity')
+
+@app.route('/activitydelete/button', methods=["GET", "POST"])
+def activitydeleteButton(): 
     activity = Activities.query.first()
     db.session.delete(activity)
     db.session.commit()
